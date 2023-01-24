c274071e531216fd23b8ef07743cdf09a48e055e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-01-23 20:24:32 -0400

    created a new window

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello world 2");
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+class Window {
+    constructor() {
+    }
+    initailizedWindow() {
+        const win = new electron_1.BrowserWindow({
+            width: 800,
+            height: 600
+        });
+    }
+    createWindow() {
+        electron_1.app.whenReady().then(this.initailizedWindow);
+    }
+}
+const window = new Window();
+window.createWindow();
--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-console.log("Hello world 2");
+import electron, { app, BrowserWindow } from "electron";
+
+
+class Window {
+
+    public constructor() {
+
+    }
+
+    private initailizedWindow(): void {
+        const win = new BrowserWindow({
+            width: 800,
+            height: 600
+        })
+    }
+
+    public createWindow(): void {
+        app.whenReady().then(this.initailizedWindow)
+    }
+}
+
+const window = new Window();
+window.createWindow()
